9a95c9d67b56330ace26b1767d60effcb096163e
saix511 <saadirfan41@gmail.com>
2023-02-05 00:48:36 +0500

    fixed

--- src/app/course/[course]/page.tsx
+++ src/app/course/[course]/page.tsx
@@ -5,12 +5,6 @@ import { FC } from 'react'
 interface pageProps {
   params: { course: string }
 }
-const stats = [
-  { label: 'Founded', value: '2021' },
-  { label: 'Employees', value: '5' },
-  { label: 'Beta Users', value: '521' },
-  { label: 'Raised', value: '$25M' },
-]
 
 const page: FC<pageProps> = ({ params }) => {
   const course = courses.filter((course) => course.shortName === params.course)
@@ -58,13 +52,12 @@ const page: FC<pageProps> = ({ params }) => {
             </svg>
           </div>
           <div className='relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-none lg:px-0 lg:py-20'>
-            {/* Testimonial card*/}
             <div className='relative overflow-hidden rounded-2xl pt-64 pb-10 shadow-xl'>
               <Image
                 width={500}
                 height={500}
                 className='absolute inset-0 h-full w-full object-cover'
-                src={course[0].imageUrl}
+                src={course[0]?.imageUrl}
                 alt='LOADING IMAGE'
               />
             </div>
@@ -75,10 +68,10 @@ const page: FC<pageProps> = ({ params }) => {
           {/* Content area */}
           <div className='pt-12 sm:pt-16 lg:pt-20'>
             <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-              {course[0].title}
+              {course[0]?.title}
             </h2>
             <div className='mt-6 space-y-6 text-gray-500'>
-              <p className='text-lg leading-7'>{course[0].details}</p>
+              <p className='text-lg leading-7'>{course[0]?.details}</p>
             </div>
           </div>
         </div>
@@ -86,14 +79,14 @@ const page: FC<pageProps> = ({ params }) => {
       <div className='container mx-auto h-full w-full pt-10'>
         <h1 className='mt-4 text-center text-4xl font-bold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl'>
           <span className=' bg-cyan-800 bg-clip-text text-transparent  underline sm:pb-5 lg:pb-0'>
-            {course[0].title} Road Map
+            {course[0]?.title} Road Map
           </span>
         </h1>
 
         <div className='wrap relative h-full overflow-hidden p-10'>
           <div className='lg:border-2-2  lg:absolute lg:left-[50%] lg:h-full lg:border lg:border-gray-700 lg:border-opacity-20' />
 
-          {course[0].courseTimeline.map((timeline, index) => (
+          {course[0]?.courseTimeline.map((timeline, index) => (
             <div
               key={index}
               className={` lg:mb-8 lg:flex lg:w-full ${
@@ -108,33 +101,17 @@ const page: FC<pageProps> = ({ params }) => {
               </div>
               <div className=' mb-5 w-full rounded-lg p-7 px-10 shadow-xl lg:w-5/12'>
                 <p className='bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text text-lg font-medium leading-snug tracking-wide text-opacity-100 '>
-                  {timeline.tag}
+                  {timeline?.tag}
                 </p>
                 <h3 className='mb-3 text-xl font-bold text-black'>
-                  {timeline.heading}
+                  {timeline?.heading}
                 </h3>
                 <p className='text-sm font-semibold leading-snug tracking-[.1rem] text-opacity-100'>
-                  {timeline.preview}
+                  {timeline?.preview}
                 </p>
               </div>
             </div>
           ))}
-
-          {/* <div className='left-timeline mb-8 flex w-full flex-row-reverse items-center justify-between'>
-            <div className=' w-5/12'></div>
-            <div className='z-20  flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl'>
-              <h1 className='mx-auto text-lg font-semibold text-white'>2</h1>
-            </div>
-            <div className=' w-5/12 rounded-lg bg-red-400 px-6 py-4 shadow-xl'>
-              <h3 className='mb-3 text-xl font-bold text-white'>Lorem Ipsum</h3>
-              <p className='text-sm font-medium leading-snug tracking-wide text-white text-opacity-100'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
